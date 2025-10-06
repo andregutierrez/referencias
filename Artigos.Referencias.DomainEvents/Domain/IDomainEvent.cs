fae7aaa5fdd051fc1601d94e11aa079aa2b6c21b
andregutierrez <andregutierrez@gmail.com>
2025-10-06 18:27:00 -0300

    Delete Artigos.Referencias.DomainEvents directory

--- Artigos.Referencias.DomainEvents/Domain/IDomainEvent.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Artigos.Referencias.DomainEvents.Domain;
-
-/// <summary>
-/// Marcador para todos os eventos de domínio.
-/// Eventos de domínio representam fatos importantes que ocorreram no agregado.
-/// </summary>
-public interface IDomainEvent { }
